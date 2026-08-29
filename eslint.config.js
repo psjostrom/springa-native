eb6613c94d8397335f404c058f637f9f41da348d
Per Sjöström <persinternetpost@gmail.com>
2026-08-29 10:47:35 +0200

    build: configure react compiler and eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const reactCompiler = require('eslint-plugin-react-compiler');
 
 module.exports = defineConfig([
   expoConfig,
+  {
+    plugins: {
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      'react-compiler/react-compiler': 'error',
+    },
+  },
   {
     ignores: ['dist/*'],
   },
